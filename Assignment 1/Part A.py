f7593ae8998ce5df2543bfbec787567fee14713c
A-Desai-92 <38077427+A-Desai-92@users.noreply.github.com>
2018-04-27 11:08:09 +0100

    Update Part A.py

--- Assignment 1/Part A.py
+++ Assignment 1/Part A.py
@@ -9,14 +9,16 @@
 
 print('Welcome to Astha\'s Calculator')
 print('Part A - Add two numbers')
+def PartA)
 print('The sum is %.1f' %(float(input('Enter first number: '))+float(input('Enter second number: '))))
 
 def main():
+  PartA()
   while(True):
     again = raw_input("Sweet! Would you like to try again? ")
     
     if again.lower().startswith("y"):
-      partB()
+      partA()
     
     elif again.lower().startswith("n"):
       print("See you later!")
